75bea62183bcad36bf9194f3a78588c1c2ff6785
Stephan Pleines <pleines.stephan@gmail.com>
2021-01-18 16:42:58 -0800

    WIP for modes

--- imx219_modes.h
+++ imx219_modes.h
@@ -33,80 +33,96 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // REQUESTS FOR SUPPORT ABOUT THESE REGISTER VALUES WILL
 // BE IGNORED BY PI TOWERS.
 
+
+  // /* Handle crop */
+  // ret = reg_write(client, 0x0164, priv->crop_rect.left >> 8);
+  // ret |= reg_write(client, 0x0165, priv->crop_rect.left & 0xff);
+  // ret |= reg_write(client, 0x0166, (priv->crop_rect.width - 1) >> 8);
+  // ret |= reg_write(client, 0x0167, (priv->crop_rect.width - 1) & 0xff);
+  // ret |= reg_write(client, 0x0168, priv->crop_rect.top >> 8);
+  // ret |= reg_write(client, 0x0169, priv->crop_rect.top & 0xff);
+  // ret |= reg_write(client, 0x016A, (priv->crop_rect.height - 1) >> 8);
+  // ret |= reg_write(client, 0x016B, (priv->crop_rect.height - 1) & 0xff);
+  // ret |= reg_write(client, 0x016C, priv->crop_rect.width >> 8);
+  // ret |= reg_write(client, 0x016D, priv->crop_rect.width & 0xff);
+  // ret |= reg_write(client, 0x016E, priv->crop_rect.height >> 8);
+  // ret |= reg_write(client, 0x016F, priv->crop_rect.height & 0xff);
+
+
 #ifndef IMX219MODES_H_
 #define IMX219MODES_H_
-
 struct sensor_regs imx219_8MPix[] =
 {
-      /* 3280x2464@15 FPS */
-      {0x0100, 0x00},       // 0=OFF, 1=Stream, 2=MAX
-      {0x30EB, 0x05},
-      {0x30EB, 0x0C},
-      {0x300A, 0xFF},
-      {0x300B, 0xFF},
-      {0x30EB, 0x05},
-      {0x30EB, 0x09},
-      {0x0114, 0x01},      // CSI MIPI Lanes [1:0]  (0x01=2, 0x03=4)
-      {0x0128, 0x00},      // DPHY_CNTRL
-      {0x012A, 0x18},      // EXCK_FREQ [15:8]
-      {0x012B, 0x00},      // EXCK_FREQ [7:0]
-      {0x0157, 0x00},         // Analog Gain
-      {0x0158, 0x01},         // Digital Gain [15:8]
-      {0x0159, 0x00},          // Digital Gain [7:0]
-      {0x015A, 0x01},      // Shutter/Integration Time [15:8]
-      {0x015B, 0x00},      // Shutter/Integration Time [7:0]
-      {0x0160, 0x09},      // Frame Length [15:8]
-      {0x0161, 0xC8},      // Frame Length [7:0]
-      {0x0162, 0x0D},      // Line Length [15:8]
-      {0x0163, 0x78},      // Line Length [7:0]
-      {0x0164, 0x00},
-      {0x0165, 0x00},
-      {0x0166, 0x0C},
-      {0x0167, 0xCF},
-      {0x0168, 0x00},
-      {0x0169, 0x00},
-      {0x016A, 0x09},
-      {0x016B, 0x9F},
-      {0x016C, 0x0C},
-      {0x016D, 0xD0},
-      {0x016E, 0x09},
-      {0x016F, 0xA0},
-      {0x0170, 0x01},      // X_ODD_INC [2:0]
-      {0x0171, 0x01},      // Y_ODD_INC [2:0]
-      {0x0172, 0x03},
-      {0x0174, 0x00},      // Binning Mode H_A
-      {0x0175, 0x00},      // Binning Mode V_A
-      {0x018C, 0x0A},      // CSI Data Format [15:8]
-      {0x018D, 0x0A},      // CSI Data Format [7:0]
-      {0x0301, 0x05},      // VTPXCK_DIV
-      {0x0303, 0x01},      // VTSYCK_DIV
-      {0x0304, 0x03},      // PREPLLCK_VT_DIV [3:0]
-      {0x0305, 0x03},      // PREPLLCK_OP_DIV [3:0]
-      {0x0306, 0x00},      // PLL_VT_MPY [10:8]
-      {0x0307, 0x2B},      // PLL_VT_MPY [7:0]
-      {0x0309, 0x0A},      // OPPXCK_DIV [4:0]
-      {0x030B, 0x01},      // OPSYCK_DIV
-      {0x030C, 0x00},      // PLL_OP_MPY [10:8]
-      {0x030D, 0x55},      // PLL_OP_MPY [7:0]
-      {0x455E, 0x00},      // CIS Tuning ?
-      {0x471E, 0x4B},      // CIS Tuning ?
-      {0x4767, 0x0F},      // CIS Tuning ?
-      {0x4750, 0x14},      // CIS Tuning ?
-      {0x4540, 0x00},      // CIS Tuning ?
-      {0x47B4, 0x14},      // CIS Tuning ?
-      {0x4713, 0x30},      // CIS Tuning ?
-      {0x478B, 0x10},      // CIS Tuning ?
-      {0x478F, 0x10},      // CIS Tuning ?
-      {0x4797, 0x0E},      // CIS Tuning ?
-      {0x479B, 0x0E},      // CIS Tuning ?
-      {0x0100, 0x01}
-};
+    /* 3280x2464@15 FPS */
+    {0x0100, 0x00},  // 0=OFF, 1=Stream, 2=MAX
+    {0x30EB, 0x05},  //
+    {0x30EB, 0x0C},  //
+    {0x300A, 0xFF},  //
+    {0x300B, 0xFF},  //
+    {0x30EB, 0x05},  //
+    {0x30EB, 0x09},  //
+    {0x0114, 0x01},  // CSI MIPI Lanes [1:0] (0x01=2, 0x03=4)
+    {0x0128, 0x00},  // DPHY_CNTRL
+    {0x012A, 0x18},  // EXCK_FREQ [15:8]
+    {0x012B, 0x00},  // EXCK_FREQ [7:0]
+    {0x0157, 0x00},  // Analog Gain
+    {0x0158, 0x01},  // Digital Gain [15:8]
+    {0x0159, 0x00},  // Digital Gain [7:0]
+    {0x015A, 0x01},  // Shutter/Integration Time [15:8]
+    {0x015B, 0x00},  // Shutter/Integration Time [7:0]
+    {0x0160, 0x09},  // Frame Length [15:8], Frame length is 0x9c8 = 2540
+    {0x0161, 0xC8},  // Frame Length [7:0]
+    {0x0162, 0x0D},  // Line Length [15:8], Line length is 0xd78 = 3448
+    {0x0163, 0x78},  // Line Length [7:0]
+    {0x0164, 0x00},  // Crop left offset [15:8] (here 0 for full frame).
+    {0x0165, 0x00},  // Crop left offset [7:0] 
+    {0x0166, 0x0C},  // Crop width-1 [15:8] (here full width - 1).
+    {0x0167, 0xCF},  // Crop width-1 [7:0]
+    {0x0168, 0x00},  // Crop top offset [15:8] (here 0 for full frame).
+    {0x0169, 0x00},  // Crop top offset [7:0]
+    {0x016A, 0x09},  // Crop height-1 [15:8] (here full height - 1).
+    {0x016B, 0x9F},  // Crop height-1 [7:0]
+    {0x016C, 0x0C},  // Image width [15:8] (here 0xcd0 = 3280 pixels).
+    {0x016D, 0xD0},  // Image width [7:0]
+    {0x016E, 0x09},  // Image height [15:8] (here 0x9a0 = 2464 pixels).
+    {0x016F, 0xA0},  // Image height [7:0]
+    {0x0170, 0x01},  // X_ODD_INC [2:0]
+    {0x0171, 0x01},  // Y_ODD_INC [2:0]
+    {0x0172, 0x03},  // 
+    {0x0174, 0x00},  // Binning Mode H_A
+    {0x0175, 0x00},  // Binning Mode V_A
+    {0x018C, 0x0A},  // CSI Data Format [15:8]
+    {0x018D, 0x0A},  // CSI Data Format [7:0]
+    {0x0301, 0x05},  // VTPXCK_DIV
+    {0x0303, 0x01},  // VTSYCK_DIV
+    {0x0304, 0x03},  // PREPLLCK_VT_DIV [3:0]
+    {0x0305, 0x03},  // PREPLLCK_OP_DIV [3:0]
+    {0x0306, 0x00},  // PLL_VT_MPY [10:8]
+    {0x0307, 0x2B},  // PLL_VT_MPY [7:0]
+    {0x0309, 0x0A},  // OPPXCK_DIV [4:0]
+    {0x030B, 0x01},  // OPSYCK_DIV
+    {0x030C, 0x00},  // PLL_OP_MPY [10:8]
+    {0x030D, 0x55},  // PLL_OP_MPY [7:0]
+    {0x4540, 0x00},  // CIS Tuning ?
+    {0x455E, 0x00},  // CIS Tuning ?
+    {0x4713, 0x30},  // CIS Tuning ?
+    {0x471E, 0x4B},  // CIS Tuning ?
+    {0x4750, 0x14},  // CIS Tuning ?
+    {0x4767, 0x0F},  // CIS Tuning ?
+    {0x478B, 0x10},  // CIS Tuning ?
+    {0x478F, 0x10},  // CIS Tuning ?
+    {0x4797, 0x0E},  // CIS Tuning ?
+    {0x479B, 0x0E},  // CIS Tuning ?
+    {0x47B4, 0x14},  // CIS Tuning ?
+    {0x0100, 0x01}};
+
 
 // Register setting provided on
 // https://www.raspberrypi.org/forums/viewtopic.php?f=43&t=109137&start=350#p1259865
 // with the addreg macro removed.
 struct sensor_regs imx219_mode1[] =
 {
+      // 1920x1080
       {0x0100, 0x00},
       {0x30eb, 0x05},
       {0x30eb, 0x0c},
@@ -160,7 +176,6 @@ struct sensor_regs imx219_mode1[] =
       {0x4793, 0x10},
       {0x4797, 0x0e},
       {0x479b, 0x0e},
-
       {0x0172, 0x03},
       {0x0157, 0x00},
       {0x0160, 0x06},
@@ -174,6 +189,7 @@ struct sensor_regs imx219_mode1[] =
 
 struct sensor_regs imx219_mode2[] =
 {
+      // 3280x2464
       {0x0100, 0x00},
       {0x30eb, 0x0c},
       {0x30eb, 0x05},
@@ -243,6 +259,7 @@ struct sensor_regs imx219_mode2[] =
 
 struct sensor_regs imx219_mode3[] =
 {
+      // 3280x2464
       {0x0100, 0x00},
       {0x30eb, 0x0c},
       {0x30eb, 0x05},
@@ -254,6 +271,13 @@ struct sensor_regs imx219_mode3[] =
       {0x0128, 0x00},
       {0x012a, 0x18},
       {0x012b, 0x00},
+      {0x0157, 0x00},
+      {0x015a, 0x00},
+      {0x015b, 0x31},
+      {0x0160, 0x0d},
+      {0x0161, 0xc6},
+      {0x0162, 0x0d},
+      {0x0163, 0x78},
       {0x0164, 0x00},
       {0x0165, 0x00},
       {0x0166, 0x0c},
@@ -268,6 +292,7 @@ struct sensor_regs imx219_mode3[] =
       {0x016f, 0xa0},
       {0x0170, 0x01},
       {0x0171, 0x01},
+      {0x0172, 0x03},
       {0x0174, 0x00},
       {0x0175, 0x00},
       {0x018c, 0x0a},
@@ -286,32 +311,24 @@ struct sensor_regs imx219_mode3[] =
       {0x0625, 0xd0},
       {0x0626, 0x09},
       {0x0627, 0xa0},
-      {0x455e, 0x00},
-      {0x471e, 0x4b},
-      {0x4767, 0x0f},
-      {0x4750, 0x14},
       {0x4540, 0x00},
-      {0x47b4, 0x14},
+      {0x455e, 0x00},
       {0x4713, 0x30},
+      {0x471e, 0x4b},
+      {0x4750, 0x14},
+      {0x4767, 0x0f},
       {0x478b, 0x10},
       {0x478f, 0x10},
       {0x4793, 0x10},
       {0x4797, 0x0e},
       {0x479b, 0x0e},
-
-      {0x0172, 0x03},
-      {0x0157, 0x00},
-      {0x0160, 0x0d},
-      {0x0161, 0xc6},
-      {0x0162, 0x0d},
-      {0x0163, 0x78},
-      {0x015a, 0x00},
-      {0x015b, 0x31},
+      {0x47b4, 0x14},
       {0x0100, 0x01},
 };
 
 struct sensor_regs imx219_mode4[] =
 {
+      // 1640x1232, 2x2 binning
       {0x0100, 0x00},
       {0x30eb, 0x0c},
       {0x30eb, 0x05},
@@ -377,6 +394,7 @@ struct sensor_regs imx219_mode4[] =
 
 struct sensor_regs imx219_mode5[] =
 {
+      // 1640x922 with 2x2 binning
       {0x0100, 0x00},
       {0x30eb, 0x0c},
       {0x30eb, 0x05},
@@ -441,6 +459,7 @@ struct sensor_regs imx219_mode5[] =
 
 struct sensor_regs imx219_mode6[] =
 {
+      // 1280x720 with 2x2 binning
       {0x0100, 0x00},
       {0x30eb, 0x05},
       {0x30eb, 0x0c},
@@ -505,66 +524,69 @@ struct sensor_regs imx219_mode6[] =
 
 struct sensor_regs imx219_mode7[] =
 {
-      {0x0100, 0x00},
-      {0x30eb, 0x05},
-      {0x30eb, 0x0c},
-      {0x300a, 0xff},
-      {0x300b, 0xff},
-      {0x30eb, 0x05},
-      {0x30eb, 0x09},
-      {0x0114, 0x01},
-      {0x0128, 0x00},
-      {0x012a, 0x18},
-      {0x012b, 0x00},
-      {0x0164, 0x03},
-      {0x0165, 0xe8},
-      {0x0166, 0x08},
-      {0x0167, 0xe7},
-      {0x0168, 0x02},
-      {0x0169, 0xf0},
-      {0x016a, 0x06},
-      {0x016b, 0xaf},
-      {0x016c, 0x02},
-      {0x016d, 0x80},
-      {0x016e, 0x01},
-      {0x016f, 0xe0},
-      {0x0170, 0x01},
-      {0x0171, 0x01},
-      {0x0174, 0x03},
-      {0x0175, 0x03},
-      {0x018c, 0x0a},
-      {0x018d, 0x0a},
-      {0x0301, 0x05},
-      {0x0303, 0x01},
-      {0x0304, 0x03},
-      {0x0305, 0x03},
-      {0x0306, 0x00},
-      {0x0307, 0x39},
-      {0x0309, 0x0a},
-      {0x030b, 0x01},
-      {0x030c, 0x00},
-      {0x030d, 0x72},
-      {0x455e, 0x00},
-      {0x471e, 0x4b},
-      {0x4767, 0x0f},
-      {0x4750, 0x14},
-      {0x4540, 0x00},
-      {0x47b4, 0x14},
-      {0x4713, 0x30},
-      {0x478b, 0x10},
-      {0x478f, 0x10},
-      {0x4793, 0x10},
-      {0x4797, 0x0e},
-      {0x479b, 0x0e},
-      {0x0172, 0x03},
-      {0x0157, 0x00},
-      {0x0160, 0x02},
-      {0x0161, 0x39},
-      {0x0162, 0x0d},
-      {0x0163, 0xe7},
-      {0x015a, 0x00},
-      {0x015b, 0x2f},
-      {0x0100, 0x01},
+    // 640x480 with 2x2 binning
+    {0x0100, 0x00},  //
+    {0x30eb, 0x05},  //
+    {0x30eb, 0x0c},  //
+    {0x300a, 0xff},  //
+    {0x300b, 0xff},  //
+    {0x30eb, 0x05},  //
+    {0x30eb, 0x09},  //
+    {0x0114, 0x01},  // CSI MIPI Lanes [1:0] (0x01=2, 0x03=4)
+    {0x0128, 0x00},  // DPHY_CNTRL
+    {0x012a, 0x18},  // EXCK_FREQ [15:8]
+    {0x012b, 0x00},  // EXCK_FREQ [7:0]
+    {0x0157, 0x00},  // Analog Gain
+                     // Digital Gain [15:8]
+                     // Digital Gain [7:0]
+    {0x015a, 0x00},  // Shutter/Integration Time [15:8]
+    {0x015b, 0x2f},  // Shutter/Integration Time [7:0]
+    {0x0160, 0x02},  // Frame Length [15:8], Frame length is 0x239 = 569 pixels
+    {0x0161, 0x39},  // Frame Length [7:0]
+    {0x0162, 0x0d},  // Line Length [15:8], Line length is 0xde7 = 3559 pixels
+    {0x0163, 0xe7},  // Line Length [7:0]
+    {0x0164, 0x03},  // Crop left offset (here 0x3e8 = 1000).
+    {0x0165, 0xe8},  //
+    {0x0166, 0x08},  // Crop width-1 (here 0x8e7 = 2279 - that doesn't make sense - maybe because of the binning?).
+    {0x0167, 0xe7},  //
+    {0x0168, 0x02},  // Crop top offset (here 0x2f0 = 752).
+    {0x0169, 0xf0},  //
+    {0x016a, 0x06},  // Crop height-1 (here 0x6af = 1711 - that doesn't make sense - maybe because of the binning?).
+    {0x016b, 0xaf},  //
+    {0x016c, 0x02},  // Image width is 0x280 = 640 || x_output_size [11:8] and [7:0]
+    {0x016d, 0x80},  //
+    {0x016e, 0x01},  // Image height is 0x1e0 = 480 || y_output_size [11:8] and [7:0]
+    {0x016f, 0xe0},  //
+    {0x0170, 0x01},  // X_ODD_INC [2:0]
+    {0x0171, 0x01},  // Y_ODD_INC [2:0]
+    {0x0172, 0x03},  //
+    {0x0174, 0x03},  // Binning Mode H_A
+    {0x0175, 0x03},  // Binning Mode V_A
+    {0x018c, 0x0a},  // CSI Data Format [15:8]
+    {0x018d, 0x0a},  // CSI Data Format [7:0]
+    {0x0301, 0x05},  // VTPXCK_DIV
+    {0x0303, 0x01},  // VTSYCK_DIV
+    {0x0304, 0x03},  // PREPLLCK_VT_DIV [3:0]
+    {0x0305, 0x03},  // PREPLLCK_OP_DIV [3:0]
+    {0x0306, 0x00},  // PLL_VT_MPY [10:8]
+    {0x0307, 0x39},  // PLL_VT_MPY [7:0]
+    {0x0309, 0x0a},  // OPPXCK_DIV [4:0]
+    {0x030b, 0x01},  // OPSYCK_DIV
+    {0x030c, 0x00},  // PLL_OP_MPY [10:8]
+    {0x030d, 0x72},  // PLL_OP_MPY [7:0]
+    {0x4540, 0x00},  //
+    {0x455e, 0x00},  //
+    {0x4713, 0x30},  //
+    {0x471e, 0x4b},  //
+    {0x4750, 0x14},  //
+    {0x4767, 0x0f},  //
+    {0x478b, 0x10},  //
+    {0x478f, 0x10},  //
+    {0x4793, 0x10},  //
+    {0x4797, 0x0e},  //
+    {0x479b, 0x0e},  //
+    {0x47b4, 0x14},  //
+    {0x0100, 0x01},  //
 };
 
 struct mode_def imx219_modes[] = {
@@ -704,6 +726,44 @@ struct sensor_regs imx219_stop[] = {
       {0x0100, 0x00},          /* disable streaming  */
 };
 
+static int imx219_set_crop(const struct sensor_def * const sensor,
+                           struct mode_def * const sensor_mode,
+                           const struct raspiraw_crop * const cfg) {
+  if (cfg->hinc >= 0) {
+    modReg(sensor_mode, 0x0170, 0, 2, cfg->hinc, EQUAL);
+  }
+
+  if (cfg->vinc >= 0) {
+    modReg(sensor_mode, 0x0171, 0, 2, cfg->vinc, EQUAL);
+  }
+
+  if (cfg->width > 0) {
+    sensor_mode->width = cfg->width;
+    modReg(sensor_mode, 0x016C, 0, 3, cfg->width >> 8, EQUAL);
+    modReg(sensor_mode, 0x016D, 0, 7, cfg->width & 0xFF, EQUAL);
+  }
+
+  if (cfg->height > 0) {
+    sensor_mode->height = cfg->height;
+    modReg(sensor_mode, 0x016E, 0, 3, cfg->height >> 8, EQUAL);
+    modReg(sensor_mode, 0x016F, 0, 7, cfg->height & 0xFF, EQUAL);
+  }
+
+  // if (cfg->left > 0) {
+  //   int val = cfg->left * sensor_mode->binning;
+  //   modReg(sensor_mode, 0x0164, 0, 3, val >> 8, EQUAL);
+  //   modReg(sensor_mode, 0x0165, 0, 7, val & 0xFF, EQUAL);
+  // }
+
+  // if (cfg->top > 0) {
+  //   int val = cfg->top * sensor_mode->binning;
+  //   modReg(sensor_mode, 0x0168, 0, 3, val >> 8, EQUAL);
+  //   modReg(sensor_mode, 0x0169, 0, 7, val & 0xFF, EQUAL);
+  // }
+
+  return 0;
+}
+
 // ID, exposure, and gain register settings taken from
 // https://android.googlesource.com/kernel/bcm/+/android-bcm-tetra-3.10-lollipop-wear-release/drivers/media/video/imx219.c
 // Flip settings taken from https://github.com/rellimmot/Sony-IMX219-Raspberry-Pi-V2-CMOS/blob/master/imx219mipiraw_Sensor.c#L585
@@ -733,6 +793,8 @@ struct sensor_def imx219 = {
 
       .gain_reg =             0x0157,
       .gain_reg_num_bits =    8,    //Only valid up to 230.
+
+      .set_crop = imx219_set_crop,
 };
 
 #endif
